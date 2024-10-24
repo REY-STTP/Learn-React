9a92c3b1b94cf1c6657981f1645a1277fd8300c3
REY-STTP <rey.zakaria123@gmail.com>
2024-10-24 09:41:54 +0700

    update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,26 @@
 import React from "react";
-import { Component } from "react";
+
+const Button = (props) => {
+  const { children = "...", variant = "bg-black" } = props
+  return (
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
+
 function App() {
-  class Button extends React.Component {
-    render(){
-      return(
-        <button className="h-10 px-6 font-semibold rounded-md bg-white text-black" type="submit">
-      Order Now
-      </button>
-      );
-    }
-  }
-function ButtonBlack() {
   return (
-    <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
-    Order Now
-      </button>
-  );
-}
-function ButtonRed() {
-  return (
-    <button className="h-10 px-6 font-semibold rounded-md bg-red-500 text-white" type="submit">
-    Order Now
-      </button>
-  );
-}
-  return (
-    <div className="flex justify-center bg-cyan-100 min-h-screen items-center">
-      <div>
+    <div className="flex justify-center bg-green-400 min-h-screen items-center">
+      <div className="flex gap-x-3">
+      <Button variant="bg-red-500">WIN Filter</Button>
+      <Button variant="bg-blue-600">Order Now</Button>
       <Button></Button>
+      <Button>CLick</Button>
       <Button></Button>
-      <ButtonBlack></ButtonBlack>
-      <ButtonRed></ButtonRed>
       </div>
       
     </div>
